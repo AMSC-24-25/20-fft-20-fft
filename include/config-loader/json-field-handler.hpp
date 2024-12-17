f9bc84e7c2062d95b61d93017d094dc5fd0246f0
AndreVale69 <sforzini31@gmail.com>
2024-12-17 14:16:50 +0100

    feat(JSONFieldHandler): add utility class
    JsonFieldHandler class is a utility class that provides
    field names for the JSON configuration file.

---
+++ include/config-loader/json-field-handler.hpp
@@ -0,0 +1,44 @@
+#ifndef JSON_FIELD_HANDLER_HPP
+#define JSON_FIELD_HANDLER_HPP
+
+#include <string>
+#include <unordered_map>
+
+/**
+ * JsonFieldHandler class is a utility class that provides field names for the JSON configuration file.
+ */
+class JsonFieldHandler {
+public:
+  /**
+   * Enum class for the field names in the JSON configuration file.
+   */
+  enum class Field {
+    SignalDomain,
+    SignalLength,
+    HzFrequency,
+    Phase,
+    Noise
+  };
+
+  /**
+   * Returns true if the field is a valid field in the JSON configuration file.
+   * @param field The field name as a string.
+   * @return True if the field is valid, false otherwise.
+   */
+  static bool hasField(const std::string &field);
+
+  /**
+   * Get the field name from the field enum.
+   * @param field The field enum.
+   * @return The field name.
+   */
+  static std::string getFieldName(Field field);
+
+private:
+  /**
+   * Unordered map that maps the field enum to the field name.
+   */
+  static const std::unordered_map<Field, std::string> fieldNames;
+};
+
+#endif //JSON_FIELD_HANDLER_HPP
---
+++ src/config-loader/json-field-handler.cpp
@@ -0,0 +1,37 @@
+#include "config-loader/json-field-handler.hpp"
+
+#include <stdexcept>
+#include <bits/ranges_algo.h>
+
+const std::unordered_map<JsonFieldHandler::Field, std::string> JsonFieldHandler::fieldNames = {
+    {Field::SignalDomain, "signal_domain"},
+    {Field::SignalLength, "signal_length"},
+    {Field::HzFrequency, "hz_frequency"},
+    {Field::Phase, "phase"},
+    {Field::Noise, "noise"}
+};
+
+bool JsonFieldHandler::hasField(const std::string &field) {
+    /**
+     * Find at least one element in the range that satisfies the predicate (lambda function):
+     * the value of the pair is equal to the field.
+     * - range: fieldNames is an unordered_map,
+     *          so it uses std::ranges::begin(fieldNames) and std::ranges::end(fieldNames)
+     * - pred: lambda function that checks whether the second element (the value)
+     *         of the pair is equal to the field
+     */
+    return std::ranges::any_of(fieldNames, [&field](const std::pair<Field, std::string> &f) -> bool {
+        return f.second == field;
+    });
+}
+
+std::string JsonFieldHandler::getFieldName(const Field field) {
+    // type: std::unordered_map<Field, std::string>::const_iterator
+    // note: find() returns an iterator to the element if found,
+    //       otherwise it returns an iterator to the end of the container;
+    //       it is a sentinel value
+    if (const auto field_name = fieldNames.find(field); field_name != fieldNames.end()) {
+        return field_name->second;
+    }
+    throw std::invalid_argument("Field not found");
+}
