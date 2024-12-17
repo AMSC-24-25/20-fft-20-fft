dacb9c5736d58b4ce360ba4d67e371dfff91c3b4
AndreVale69 <sforzini31@gmail.com>
2024-12-17 14:48:47 +0100

    refactor(JSONConfigurationLoader): getter cleanup

--- include/config-loader/json-configuration-loader.hpp
+++ include/config-loader/json-configuration-loader.hpp
@@ -2,60 +2,23 @@
 #define JSON_CONFIGURATION_LOADER_HPP
 
 #include <nlohmann/json.hpp>
+
+#include "json-field-handler.hpp"
 #include "config-loader/abstract-configuration-loader.hpp"
 
 /**
  * JSONConfigurationLoader is a concrete class that implements the AbstractConfigurationLoader interface.
  */
 class JSONConfigurationLoader final : public AbstractConfigurationLoader {
-    nlohmann::json configurationData = nlohmann::json::value_t::discarded;
+    std::optional<JsonFieldHandler> configurationData;
 
 public:
     /**
      * Get the configuration data that was loaded.
-     * @return The configuration data.
-     * @throw std::runtime_error If the configuration data is empty.
+     * @return The configuration data as a JsonFieldHandler object.
+     * @throw std::runtime_error If no configuration data was loaded.
      */
-    [[nodiscard]] const nlohmann::json &getConfigurationData() const;
-
-    /**
-     * Get the signal domain from the configuration data.
-     * @return The signal domain.
-     * @throw std::runtime_error If the signal domain is null.
-     */
-    [[nodiscard]] std::string getSignalDomain() const;
-
-    /**
-     * Get the signal length from the configuration data.
-     * @return The signal length.
-     * @throw std::runtime_error If the signal length is null.
-     */
-    [[nodiscard]] int getSignalLength() const;
-
-    /**
-     * The lower bound for the uniform distribution.
-     * @return The lower bound for the uniform distribution.
-     */
-    [[nodiscard]] int getLowerBound() const;
-
-    /**
-     * The upper bound for the uniform distribution.
-     * @return The upper bound for the uniform distribution.
-     */
-    [[nodiscard]] int getUpperBound() const;
-
-    /**
-     * Get the seed from the configuration data.
-     * @return The seed.
-     * @throw std::runtime_error If the seed is null.
-     */
-    [[nodiscard]] int getSeed() const;
-
-    /**
-     * Check if the configuration data has a seed.
-     * @return True if the configuration data has a seed; false otherwise.
-     */
-    [[nodiscard]] bool hasSeed() const;
+    [[nodiscard]] const JsonFieldHandler &getConfigurationData() const;
 
     /**
      * Given a file path to a JSON file, load the configuration from the file.
--- src/config-loader/json-configuration-loader.cpp
+++ src/config-loader/json-configuration-loader.cpp
@@ -4,53 +4,15 @@
 
 #include <fstream>
 #include <config-loader/json-configuration-loader.hpp>
+#include <config-loader/json-field-handler.hpp>
 
 using json = nlohmann::json;
 
-const nlohmann::json &JSONConfigurationLoader::getConfigurationData() const {
-    if (configurationData.is_discarded()) {
-        throw std::runtime_error("Configuration data is empty.");
+const JsonFieldHandler &JSONConfigurationLoader::getConfigurationData() const {
+    if (configurationData.has_value()) {
+        return configurationData.value();
     }
-    return configurationData;
-}
-
-std::string JSONConfigurationLoader::getSignalDomain() const {
-    if (configurationData.at("signal_domain").is_null()) {
-        throw std::runtime_error("Signal domain is null.");
-    }
-    return configurationData.at("signal_domain");
-}
-
-int JSONConfigurationLoader::getSignalLength() const {
-    if (configurationData.at("signal_length").is_null()) {
-        throw std::runtime_error("Signal length is null.");
-    }
-    return configurationData.at("signal_length");
-}
-
-int JSONConfigurationLoader::getLowerBound() const {
-    if (configurationData.at("lower_bound").is_null()) {
-        throw std::runtime_error("Lower bound is null.");
-    }
-    return configurationData.at("lower_bound");
-}
-
-int JSONConfigurationLoader::getUpperBound() const {
-    if (configurationData.at("upper_bound").is_null()) {
-        throw std::runtime_error("Upper bound is null.");
-    }
-    return configurationData.at("upper_bound");
-}
-
-int JSONConfigurationLoader::getSeed() const {
-    if (configurationData.at("seed").is_null()) {
-        throw std::runtime_error("Seed is null.");
-    }
-    return configurationData.at("seed");
-}
-
-bool JSONConfigurationLoader::hasSeed() const {
-    return configurationData.contains("seed");
+    throw std::runtime_error("No JSON configuration data loaded.");
 }
 
 void JSONConfigurationLoader::loadConfigurationFromFile(const std::string &filePath) {
@@ -70,11 +32,11 @@ void JSONConfigurationLoader::loadConfigurationFromFile(const std::string &fileP
     }
     // if all checks pass, load the configuration data
     try {
-        fileStream >> configurationData;
+        configurationData = JsonFieldHandler(fileStream);
     } catch (const nlohmann::json::parse_error &e) {
         throw std::runtime_error("Failed to parse JSON file: " + filePath + "; Parse error: " + e.what());
     }
-    // print the actual configuration data
+    // print the actual configuration data;
     printf("The configuration data has been loaded from the file: %s.\n%s\n\n",
-        filePath.c_str(), configurationData.dump(4).c_str());
+        filePath.c_str(), configurationData->getConfigurationLoaded().dump(4).c_str());
 }
