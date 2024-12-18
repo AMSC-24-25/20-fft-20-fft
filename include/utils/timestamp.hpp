cd671b9f8aecba2c15d7a0ab2702fd95615ac8db
AndreVale69 <sforzini31@gmail.com>
2024-12-18 10:09:45 +0100

    feat(timestamp): add readable timestamp method
    Add a utility function to get a readable timestamp string
    using a string format.

---
+++ include/utils/timestamp.hpp
@@ -0,0 +1,14 @@
+#ifndef TIMESTAMP_HPP
+#define TIMESTAMP_HPP
+#include <string>
+
+/**
+ * Create a timestamp string with the format provided.
+ * @param format The timestamp format.
+ *               See the documentation for more information:
+ *               <a href="https://en.cppreference.com/w/cpp/io/manip/put_time">put_time function</a>
+ * @return The timestamp string with the format provided.
+ */
+std::string createReadableTimestamp(const std::string &format);
+
+#endif //TIMESTAMP_HPP
