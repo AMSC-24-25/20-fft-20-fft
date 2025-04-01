9cdb95985267776bc12db7b4082a15fa40b25b62
francescapieruz16 <francescadora.pieruz@mail.polimi.it>
2025-04-01 15:25:59 +0200

    Add useful scripts

---
+++ run-all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "~ Export configuration ./resources/sample-config.json ~"
+export CONFIG_FILE_PATH_FFT=${pwd}/resources/sample-config.json
+echo ""
+
+echo "~ Compile CMakeLists.txt ~"
+cmake . --log-level=WARNING || exit 1
+echo ""
+
+echo "~ Compile main ~"
+make -f ./Makefile -C . parallel_build || exit 1
+echo ""
+
+echo "~ Run Main ~"
+./main
