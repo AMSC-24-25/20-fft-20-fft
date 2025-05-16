015b12d84dc44ad1b52ee531922c476efcc2134c
AndreVale69 <sforzini31@gmail.com>
2025-05-16 13:44:56 +0200

    chore(build-examples): automatically install Ninja and OpenCV

--- build-examples.sh
+++ build-examples.sh
@@ -1,37 +1,44 @@
 #!/bin/bash
 
-echo "~ Compile CMakeLists.txt in $(pwd)/build ~"
-mkdir -p "build" && cd "build" || exit 1
-# check if ninja is installed
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+echo -e "${GREEN}~ Install ninja-build ~${NC}"
 if ! command -v ninja &> /dev/null
 then
-    echo "Ninja is not installed. It is highly recommended to install it for faster builds: sudo apt install ninja-build"
-    cmake .. --preset make-dev --log-level=ERROR || exit 1
-    cd make-dev
+    sudo apt install -y ninja-build || exit 1
 else
-    echo "Ninja is installed. Using it for faster builds."
-    cmake .. --preset ninja-dev --log-level=ERROR || exit 1
-    cd ninja-dev
+    echo "Ninja is already installed."
 fi
 echo ""
 
 
-echo "~ Compile project + examples ~"
-start_time=$(date +%s)
-if command -v ninja &> /dev/null
+echo -e "${GREEN}~ Install OpenCV ~${NC}"
+if ! dpkg -l | grep 'libopencv-dev' &> /dev/null
 then
-    ninja --quiet all || exit 1
+    echo -e "${YELLOW}OpenCV is not installed. Installing...${NC}"
+    sudo apt update || exit 1
+    sudo apt install -y libopencv-dev || exit 1
 else
-    make --silent -j $(nproc) all || exit 1
+    echo "OpenCV is already installed."
 fi
+echo ""
+
+
+echo -e "${GREEN}~ Compile CMakeLists.txt in $(pwd)/build ~${NC}"
+mkdir -p "build" && cd "build" || exit 1
+cmake .. --preset ninja-dev --log-level=ERROR || exit 1
+cd ninja-dev
+echo ""
+
+
+echo -e "${GREEN}~ Compile project + examples ~${NC}"
+start_time=$(date +%s)
+ninja --quiet all || exit 1
 end_time=$(date +%s)
 elapsed_time=$((end_time - start_time))
 echo "Built in ~$elapsed_time seconds"
+echo "Run examples using ./build/ninja-dev/examples/example-name"
 echo ""
-
-if command -v ninja &> /dev/null
-then
-    echo "Run examples using ./build/ninja-dev/examples/example-name"
-else
-    echo "Run examples using ./build/make-dev/examples/example-name"
-fi
